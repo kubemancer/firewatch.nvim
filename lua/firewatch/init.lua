4dcd67a021d633a32440a03a330f019e6a38e727
kubemancer <khalil.che26@icloud.com>
2025-10-20 19:04:43 +0200

    debug color

--- lua/firewatch/init.lua
+++ lua/firewatch/init.lua
@@ -43,6 +43,24 @@ function M.setup(opts)
 	vim.o.background = "dark"
 	vim.g.colors_name = "firewatch"
 
+	-- Terminal colors
+	vim.g.terminal_color_0 = c.black
+	vim.g.terminal_color_1 = c.red
+	vim.g.terminal_color_2 = c.green
+	vim.g.terminal_color_3 = c.yellow
+	vim.g.terminal_color_4 = c.blue
+	vim.g.terminal_color_5 = c.magenta
+	vim.g.terminal_color_6 = c.cyan
+	vim.g.terminal_color_7 = c.white
+	vim.g.terminal_color_8 = c.bright_black
+	vim.g.terminal_color_9 = c.bright_red
+	vim.g.terminal_color_10 = c.bright_green
+	vim.g.terminal_color_11 = c.bright_yellow
+	vim.g.terminal_color_12 = c.bright_blue
+	vim.g.terminal_color_13 = c.bright_magenta
+	vim.g.terminal_color_14 = c.bright_cyan
+	vim.g.terminal_color_15 = c.bright_white
+
 	local c = M.colors
 	local bg = transparent and "NONE" or c.bg
 
@@ -54,9 +72,9 @@ function M.setup(opts)
 
 		-- Cursor
 		Cursor = { fg = c.bg, bg = c.cursor },
-		CursorLine = { bg = transparent and "NONE" or "#fff" },
-		CursorColumn = { bg = transparent and "NONE" or "#fff" },
-		ColorColumn = { bg = transparent and "NONE" or "#fff" },
+		CursorLine = { bg = transparent and "NONE" or "#252831" },
+		CursorColumn = { bg = transparent and "NONE" or "#252831" },
+		ColorColumn = { bg = transparent and "NONE" or "#252831" },
 
 		-- Line numbers
 		LineNr = { fg = c.comment },
